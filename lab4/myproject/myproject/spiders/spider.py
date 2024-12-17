d0428d62d190338643abb1a9218f31a40ac06528
Vasilevykh-M <vasilevyh00@gmail.com>
2024-12-18 00:42:28 +0500

    feat(lab 5): add x_path and beautiful soap

--- lab4/myproject/myproject/spiders/spider.py
+++ lab4/myproject/myproject/spiders/spider.py
@@ -3,27 +3,20 @@ import scrapy
 class ExampleSpider(scrapy.Spider):
     name = 'example'
 
-
-    allowed_domains = ['platesmania.com']
-    start_urls = ['https://platesmania.com/gallery']
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
 
 
     def parse(self, response):
         # Ищем все элементы с изображениями номеров
-        plates = response.css('div.gal-item')
-
-        for plate in plates:
-            # Получаем ссылку на изображение и страну
-            img_url = plate.css('img::attr(src)').get()
-            country = plate.css('span.flag::attr(title)').get()
-
-            # Проверяем, что данные есть и формируем item
+        for quote in response.css('div.quote'):
             yield {
-                'image_url': img_url,
-                'country': country,
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
             }
 
-        # Для перехода к следующей странице, если они есть
+            # Переход на следующую страницу (если есть)
         next_page = response.css('li.next a::attr(href)').get()
-        if next_page:
+        if next_page is not None:
             yield response.follow(next_page, self.parse)
